caa405e8a6786294c437fd25afc4469939c2e93b
Shoontaro <tishinadw@gmail.com>
2026-09-01 02:06:52 +0300

    Добавьте файлы проекта.

---
+++ GetApiKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace WeatherConsole
+{
+    public class GetApiKey
+    {
+        string ApiKey { get; set; }
+
+        public GetApiKey()
+        {
+            var config = new ConfigurationBuilder()
+               .AddUserSecrets<Program>() // Подключаем локальные секреты
+               .AddEnvironmentVariables() // Подключаем переменные окружения для продакшена
+               .Build();
+            ApiKey = config["MyApiKey"] ?? string.Empty;
+        }
+
+        public string GetKey() => this.ApiKey;
+    }
+}
---
+++ GetWether.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using Flurl;
+//using Flurl.Http;
+using System.Net.Http.Json;
+using Microsoft.Extensions.Caching.Memory;
+
+
+namespace WeatherConsole
+{
+    public class GetWether
+    {
+        public void Get()
+        {
+
+            string apiKey = new GetApiKey().GetKey();
+
+
+        }
+    }
+}
